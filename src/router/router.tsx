4240e318bf9512585d57e7d9ece05f85613b920c
JEFF Liao <jeffliao92316046@gmail.com>
2024-05-02 14:15:02 +0800

    Feat: appbar setup

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,11 +2,13 @@ import Home from '../pages/home'
 import About from '../pages/about'
 import Contact from '../pages/contact'
 import Blog from '../pages/blog'
+import Layout from '../components/layout'
 import { Route, Routes } from 'react-router-dom'
 
 const Router = () =>{
     return (
-        <> 
+        <>
+          <Layout></Layout>
           <Routes>
             <Route  path="/" Component={Home} />
             <Route  path="/about" Component={About} />
