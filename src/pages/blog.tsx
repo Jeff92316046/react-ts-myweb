68bc0c7f037027cd853460a69d7ad2371ccaf20e
JEFF Liao <jeffliao92316046@gmail.com>
2024-05-17 00:11:27 +0800

    Fix: issue_url newline bug

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -24,23 +24,23 @@ const Blog = () => {
                                 {i['title']}
                             </Typography>
                             <Typography sx={{ m:{ xs:1,sm:2 },mx:{ xs:2,sm:3 } }} variant={"body1"} >
-                                content : {i['body']}
+                                content: {i['body']}
                             </Typography>
                             <Typography sx={{ m:{ xs:1,sm:2 },mx:{ xs:2,sm:3 } }} variant={"body1"} >
-                                Assignees :&nbsp;
-                                <Typography sx={{color:theme.palette.text.primary}} component="a" href={i['assignee']['html_url']} variant={"body1"} >
+                                Assignees:&nbsp;
+                                <Typography sx={{wordBreak:"break-word" ,color:theme.palette.text.primary}} component="a" href={i['assignee']['html_url']} variant={"body1"} >
                                     {i['assignee']['login']}
                                 </Typography>
                             </Typography>
                             <Typography sx={{ m:{ xs:1,sm:2 },mx:{ xs:2,sm:3 } }} variant={"body1"} >
-                                label :&nbsp;
-                                <Typography sx={{color:i['labels'][0]['colors']}} component="span" variant={"body1"} >
+                                label:&nbsp;
+                                <Typography sx={{wordBreak:"break-word" ,color:i['labels'][0]['colors']}} component="span" variant={"body1"} >
                                     {i['labels'][0]['name']}
                                 </Typography>
                             </Typography>
                             <Typography sx={{ m:{ xs:1,sm:2 },mx:{ xs:2,sm:3 } }} variant={"body1"} >
-                                issue url :&nbsp;
-                                <Typography sx={{color:theme.palette.text.primary}} component="a" href={i['assignee']['html_url']} variant={"body1"} >
+                                issue_url:&nbsp;
+                                <Typography  sx={{wordBreak:"break-word" ,color:theme.palette.text.primary}} component="a" href={i['assignee']['html_url']} variant={"body1"} >
                                     {i['html_url']}
                                 </Typography>
                             </Typography>
